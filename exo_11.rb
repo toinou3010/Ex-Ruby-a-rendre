e3f1667bd870a3d5bb41304fcf15c807a75f18a0
Toinou3010 <cyrilplanes33@gmail.com>
2021-04-12 00:37:24 +0200

    Ex 11

--- exo_11.rb
+++ exo_11.rb
@@ -1,15 +1,15 @@
 puts "Quelle est ton année de naissance ?"
 print ">" #affiche > mais ne renvoie pas à la ligne suivante
 number = gets.chomp #Déclare la Variable age et attend une valeur de la part de l'utilisateur
-f = 0
-for i in number.to_i..2020 
+f = -1
+for i in number.to_i..2021 
 
 
 
-print "il y a #{2020 - i}"
+print "il y a #{2021 - i} ans"
 
 f += 1
 
-puts " tu avais: #{f}"
+puts " tu avais: #{f} ans"
 
 end
