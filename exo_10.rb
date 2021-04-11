5bf705094ce40c105d8009c280ff835d7e6816d6
Toinou3010 <cyrilplanes33@gmail.com>
2021-04-12 00:32:03 +0200

    Ex 10

--- exo_10.rb
+++ exo_10.rb
@@ -2,8 +2,8 @@
 puts "Quelle est ton année de naissance ?"
 print ">" #affiche > mais ne renvoie pas à la ligne suivante
 number = gets.chomp #Déclare la Variable age et attend une valeur de la part de l'utilisateur
-f = 0
-for i in number.to_i..2020 
+f = -1
+for i in number.to_i..2021 
 
 
 
